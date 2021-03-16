e365eb7cf909f947edc55f57f4e2e47a8df70dd7
Aleksandra <zukaleksandra7@gmail.com>
2021-03-16 11:40:59 +0300

    changes in check.java

--- task5/src/main/java/com/example/task5/check.java
+++ task5/src/main/java/com/example/task5/check.java
@@ -5,21 +5,12 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "check", value = "/check")
+@WebServlet(value = "/check")
 public class check extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "true";
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("json");
         PrintWriter pw = response.getWriter();
-        pw.println("{\"success\" :" + request.getParameter(message) + " }");
-    }
-
-    public void destroy() {
+        pw.println("{\"success\": true }");
     }
 }
